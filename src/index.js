5785c32490c1ae94babda991da25474764a0babe
evladimirfv <evladimirfv@gmail.com>
2021-02-18 13:59:01 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// Modules import
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react function or class (component)
+const App = () => {
+  return <div>First React function. </div>;
+}
+
+// Render the component to the browser 
+ReactDOM.render(<App />, document.querySelector('#root'));
